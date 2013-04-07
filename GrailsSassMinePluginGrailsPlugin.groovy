6d4a909836571d4e7e370401ce66ef7d74c0f40f
fedor.belov <fedor.belov@mail.ru>
2013-04-08 00:23:51 +0400

    new version

--- GrailsSassMinePluginGrailsPlugin.groovy
+++ GrailsSassMinePluginGrailsPlugin.groovy
@@ -9,7 +9,7 @@ import static ru.gramant.ScssCompilerPluginUtils.path
 @Slf4j
 class GrailsSassMinePluginGrailsPlugin {
     // the plugin version
-    def version = "0.1.7.21"
+    def version = "0.1.7.22"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "2.0 > *"
     // resources that are excluded from plugin packaging
