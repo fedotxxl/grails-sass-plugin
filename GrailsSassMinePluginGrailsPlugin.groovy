35f7b654b4ebc5ff792201040cf4600f0cc17670
fedor.belov <fedor.belov@mail.ru>
2013-06-15 20:49:20 +0400

    change version

--- GrailsSassMinePluginGrailsPlugin.groovy
+++ GrailsSassMinePluginGrailsPlugin.groovy
@@ -14,7 +14,7 @@ class GrailsSassMinePluginGrailsPlugin {
     private static final Logger LOG = LoggerFactory.getLogger("ru.grails.GrailsSassMinePluginGrailsPlugin")
 
     // the plugin version
-    def version = "0.1.7.24"
+    def version = "0.1.7.25"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "2.0 > *"
     // resources that are excluded from plugin packaging
